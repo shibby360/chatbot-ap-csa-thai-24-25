6da9e6b15642fe06fd002a6f9d02001fe04ac690
Shivank Chhaya <shivank.chhaya@gmail.com>
2024-11-10 03:25:54 +0000

    got gui and everything working

--- Bathroom.java
+++ Bathroom.java
@@ -1,5 +1,14 @@
 public class Bathroom {
-    public static void main(String[] args) {
-        
+    private static String exhibit = "";
+    private static String direction = "";
+    public static void main(String exhibitName, String direct) {
+        exhibit = exhibitName;
+        direction = direct;
+    }
+    public static String getExhibit() {
+        return exhibit;
+    }
+    public static String getDirection() {
+        return direction;
     }
 }
