920cce871b3de219243d1554e3bdfa5f37431e39
highschooljavacoder123 <as2233@pleasantonusd.net>
2024-11-18 22:18:11 +0000

    messagfe

--- Chatbot.java
+++ Chatbot.java
@@ -10,14 +10,14 @@ public class Chatbot {
         Animal lion = new Animal("lion", "d-3");
         String[] messages = {"Sorry I don't know what you mean, I can answer anything about where the animals are, the nearest bathroom, or contact information. If not interested in any of these topics, type stop to quit.", "I apologize, I am not quite following. I am here to help with directions to animals, restroom locations, or contact details if needed. If not interested in any of these topics, type stop to quit.", "Sorry, I am not sure I understand. I can assist with finding animals, locating the nearest restroom, or providing contact information. If not interested in any of these topics, type stop to quit."};
 
-        int count = 1;
+        
         // return ("Hello there, I am ZooBot! I can help with finding the animal exhibit of your choosing, the nearest bathroom, and provide contact information.");
         // String strInput = inputs.nextLine();
         boolean recognized = false;
-        if(strInput.toLowerCase().contains("animals") || strInput.toLowerCase().contains("bathroom") || strInput.toLowerCase().contains("contact")) {
+        if(strInput.toLowerCase().contains("animal") || strInput.toLowerCase().contains("bathroom") || strInput.toLowerCase().contains("contact")) {
             recognized = true;
         }
-        if (strInput.toLowerCase().contains("animals")) {
+        if (strInput.toLowerCase().contains("animal") ) {
             animal = true;
             return ("What animals would you want to look at, Pandas, Monkeys, or Lions?");
         }
